b83861c4ad26882e4e706e83c3afd46426fc6439
Alexcss777 <51940752+Alexcss777@users.noreply.github.com>
2019-06-29 08:41:32 -0700

    Add files via upload

---
+++ claseabstracta/claseabstracta/ClsAbsClientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseabstracta
+{
+    public abstract class ClsAbsClientes
+    {
+
+        public abstract int Id { get; set; }
+
+        public abstract int Nombre { get; set; }
+
+        public abstract int Clave { get; set; }
+
+        public abstract int RFC { get; set; }
+
+        public abstract int TipoRegimen { get; set; }
+
+        public abstract int NombreContacto { get; set; }
+
+
+
+
+}
+}
